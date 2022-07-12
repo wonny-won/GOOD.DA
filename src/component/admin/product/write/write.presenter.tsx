bb48feabd953ac6c88021c9e6b6fdaba2483aa7e
wonny-won <jaake97@newbizstart.com>
2022-07-12 21:33:44 +0900

    2022-07-11: 어드민 제품 등록 화면 그리기

--- src/component/admin/product/write/write.presenter.tsx
+++ src/component/admin/product/write/write.presenter.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import * as S from './writer.style'
 
 export default function ProductWriteUI(){
     const category = ["다이어트","건강기능","운동보조","뷰티기능","한끼식사","부기차","임산부","어린이"]
@@ -10,49 +11,44 @@ export default function ProductWriteUI(){
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
 
-            <h1>상품 이미지를 등록해주세요</h1>
-            <section>
-                <input type="file"/>
-            </section>
-
-            <h1>제목과 브랜드를 입력해주세요</h1>
-            <section>
-                <h2>브랜드를 입력해주세요</h2>
+            <S.Title>상품 이미지를 등록해주세요</S.Title>
+                <S.ImgSection>
+                    <label htmlFor="fileTag"><img src="/image/plus-circle.svg"/></label>
+                    <S.Input type="file" id="fileTag" hidden={true}/>
+                </S.ImgSection>
+            <S.Title>제목과 브랜드를 입력해주세요</S.Title>
                 <section>
-                    <input type="text" placeholder="브랜드를 입력해주세요"/>
+                    <S.HiddenTitle>브랜드를 입력해주세요</S.HiddenTitle>
+                    <S.Section>
+                        <S.Input type="text" placeholder=" 브랜드를 입력해주세요"/>
+                    </S.Section>
+                    <S.HiddenTitle>상품명을 입력해주세요</S.HiddenTitle>
+                    <S.Section>
+                        <S.Input type="text" placeholder=" 상품명을 입력해주세요"/>
+                    </S.Section>
                 </section>
-                <h2>상품명을 입력해주세요</h2>
-                <section>
-                    <input type="text" placeholder="상품명을 입력해주세요"/>
-                </section>
-            </section>
-
-            <h1>가격과 카테고리를 입력해주세요</h1>
-            <section>
-                <h2>가격</h2>
-                <section>
-                    <input type="text"/>
-                </section>
-                <h2>카테고리</h2>
-                <section>
-                    <select>
-                        { // key값에 index를 준 이유는 category 배열은 변할일이 없고 변해도 개수가 8개로 정해져 있기 때문입니다.
-                          category.map((item,index)=>(<option key={index}>{item}</option>)) }
-                    </select>
-                </section>
-            </section>
-
-            <h1>상품 한줄 설명을 입력해주세요</h1>
-            <section>
-                <input type="text"/>
-            </section>
-
-            <h1>성분 구성을 입력해주세요(3가지)</h1>
-            <section>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-            </section>
+            <S.Title>가격과 카테고리를 입력해주세요</S.Title>
+                <S.CategorySection>
+                    <S.HiddenTitle>가격</S.HiddenTitle>
+                        <S.CategoryInput type="text" placeholder=" 가격을 입력해주세요"/>
+                    <S.HiddenTitle>카테고리</S.HiddenTitle>
+                        <S.CategorySelect>
+                            {
+                             // key값에 index를 준 이유는 category 배열은 변할일이 없고 변해도 개수가 8개로 정해져 있기 때문입니다.
+                             category.map((item,index)=>(<option key={index}>{item}</option>))
+                            }
+                        </S.CategorySelect>
+                </S.CategorySection>
+            <S.Title>상품 한줄 설명을 입력해주세요</S.Title>
+                <S.Section>
+                    <S.Input type="text" placeholder=" 상품 한줄 설명을 입력헤주세요"/>
+                </S.Section>
+            <S.Title>성분 구성을 입력해주세요(3가지)</S.Title>
+                <S.Section>
+                    <S.Input type="text" placeholder=" 1. 구성성분을 입력해주세요"/>
+                    <S.Input type="text" placeholder=" 2. 구성성분을 입력해주세요"/>
+                    <S.Input type="text" placeholder=" 3. 구성성분을 입력해주세요"/>
+                </S.Section>
         </>
     )
 }
---
+++ src/component/admin/product/write/writer.style.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.h1`
+    margin-top : 50px;
+    padding-left: 15px;
+    font-size : 25px;
+    font-weight: 400;
+    color : rgba(0,0,0,0.8)
+`
+export const HiddenTitle = styled.h2`
+    display: none;
+`
+export const ImgSection = styled.section`
+    width : 103px;
+    height : 103px;
+    border : 2px dotted rgba(0,0,0,0.2);
+    border-radius : 5px;
+    margin-left : 15px;
+    display : flex;
+    justify-content: center;
+    align-items: center;
+`
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const CategorySection = styled.section`
+    display: flex;
+`
+export const CategoryInput = styled.input`
+    width :40%;
+    height: 35px;
+    margin-left : 15px;
+    border : none;
+    border-bottom: 1px solid rgba(0,0,0,0.2);
+`
+export const CategorySelect = styled.select`
+    width : 40%;
+    height :35px;
+    margin-left : 25px;
+    border : 1px solid rgba(0,0,0,0.2);
+    border-radius: 4px;
+`
+export const Input = styled.input`
+    width : 90%;
+    height: 35px;
+    margin : 11px auto;
+    border : none;
+    border-bottom: 1px solid rgba(0,0,0,0.2);
+`
