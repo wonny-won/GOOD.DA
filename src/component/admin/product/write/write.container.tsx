cfbd2ba8723b7234bfca1eee1eee7e04e157ed42
wonny-won <jaake97@newbizstart.com>
2022-07-13 20:05:03 +0900

    2022-07-13:상품 등록페이지 파이어 베이스 연결 성공

--- src/component/admin/product/write/write.container.tsx
+++ src/component/admin/product/write/write.container.tsx
@@ -1,10 +1,16 @@
 import ProductWriteUI from "./write.presenter"
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { ProductInput } from "./write.types"
+import { fireBaseApp } from "../../../../../pages/_app";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
 
 export default function ProductWrite(){
     const { register, handleSubmit } = useForm<ProductInput>()
-    const onSubmit: SubmitHandler<ProductInput> = (data : any)=>{console.log(data)}
+    const onSubmit: SubmitHandler<ProductInput> = async (data : any)=>{
+        const db = getFirestore(fireBaseApp)
+        await addDoc(collection(db, "productWrite"),{...data})
+        console.log(data)
+    }
 
     return <ProductWriteUI 
                 register={register}
