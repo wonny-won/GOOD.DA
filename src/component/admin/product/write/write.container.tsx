b9ed5deb34bf570ac51bedbb425f70bf18d64127
wonny-won <jaake97@newbizstart.com>
2022-07-14 22:59:59 +0900

    2022-07-14: 이미지 업로드 url 받아오기 시도 - 백엔드 보내줘야 함

--- src/component/admin/product/write/write.container.tsx
+++ src/component/admin/product/write/write.container.tsx
@@ -2,15 +2,16 @@ import ProductWriteUI from "./write.presenter"
 import { useForm } from 'react-hook-form'
 import { fireBaseApp } from "../../../../../pages/_app";
 import { collection, addDoc, getFirestore } from "firebase/firestore";
-import { getStorage,ref,uploadBytes } from "firebase/storage";
+import { getStorage,ref,uploadBytes,getDownloadURL } from "firebase/storage";
 import { ProductInput } from "./write.types";
 import { ChangeEvent, useState } from "react";
 
 
 export default function ProductWrite(){
     const { register, handleSubmit } = useForm<ProductInput>()
-    // 이미지 업로드
+    // 이미지 업로드,URL다운로드 -> db에 보내줘야 해
     const [image,setImage] = useState("")
+    // const [imageUrl,setImageUrl] =useState("")
     const onChangeImg = (event:ChangeEvent<HTMLInputElement>)=>{
         const file = event.target.files?.[0] 
         console.log(file)
@@ -18,6 +19,7 @@ export default function ProductWrite(){
         const storage = getStorage();
         const imageUpload = ref(storage, `image/${file?.name}`); 
         uploadBytes(imageUpload, file)
+        // const url = getDownloadURL(ref(storage, `image/${file?.name}`))
     }
     console.log(image)
     // 상품 등록 함수
