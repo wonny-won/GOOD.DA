57677c814f1e5c43bd2f45caeaa31ae9798c6de5
wonny-won <jaake97@newbizstart.com>
2022-07-22 10:39:54 +0900

    2022-07-22:리스트 페이지 화면 그리기

--- src/component/admin/product/list/list.presenter.tsx
+++ src/component/admin/product/list/list.presenter.tsx
@@ -1,10 +1,15 @@
 import RankingUnits from "../../../../units/ranking/ranking";
-export default function AdminProductListUI(props){
-    console.log(props.data)
+export default function AdminProductListUI(props:any){
+    console.log(props.data[1])
     
     return(
         <>
-        뭐야 왜저래
+        {
+            props.data&& props.data.map((item:any)=>(<RankingUnits key={item.id}
+                                                        brandName={item.productInof.brandName}
+                                                        category={item.productInof.category}
+                                                        product={item.productInof.product}/>))
+        }
         </>
     )
 }
